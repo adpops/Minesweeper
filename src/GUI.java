d396a38161356a0deed15e635918e0a2ab849532
Jimmy Nimavat <jaimin2468@gmail.com>
2018-10-22 21:11:50 -0700

    did more work on remake

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,49 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.CardLayout;
 
 public class GUI 
 {
 	private JFrame frame;
+	private int frameWidth;
+	private int frameHeight;
+	private int generalTxtWidth = 150;
+	private int generalTxtHeight = 25;
+	private String loginCard = "login";
 
+	public GUI() 
+	{
+		frameWidth = 900; 
+		frameHeight = 700;
+		frame = new JFrame();
+		frame.setSize(frameWidth, frameHeight);
+		frame.setTitle("Minesweeper");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		createLoginPanel();
+	}
+
+	private void createLoginPanel() 
+	{
+		JPanel loginP = new JPanel();
+		frame.getContentPane().add(loginP, loginCard);
+		loginP.setLayout(null);
+		
+		int loginTxtWidth = generalTxtWidth;
+		int loginTxtHeight = generalTxtHeight;
+		int loginTxtX = frameWidth * 2/3 - loginTxtWidth/2;
+		//int loginTxtY = (frameHeight - )
+		JTextField loginUserTxt = new JTextField();
+	    loginUserTxt.setBounds(loginTxtX, 167, loginTxtWidth, loginTxtHeight);
+	    loginP.add(loginUserTxt);
+	    loginUserTxt.setColumns(10);
+	}
+	
 	public static void main(String[] args) 
 	{
 		EventQueue.invokeLater(new Runnable() 
@@ -24,19 +62,4 @@ public class GUI
 			}
 		});
 	}
-
-
-	public GUI() 
-	{
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		createLoginPanel();
-	}
-
-	private void createLoginPanel() 
-	{
-		
-	}
-
 }
